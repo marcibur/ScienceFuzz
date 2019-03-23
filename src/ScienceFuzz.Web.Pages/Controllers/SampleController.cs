3e7c570de597ee92c253c60ae77ea8b6c5883b61
Marcin Burak <marcin.burak@outlook.com>
2019-03-23 19:21:46 +0100

    Added sample download feature

---
+++ src/ScienceFuzz.Web.Pages/Controllers/SampleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScienceFuzz.Web.Pages.Controllers
+{
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet("/api/Samples")]
+        public FileResult GetSample()
+        {
+            return File("sciencefuzz_sample.csv", "application/csv", "sciencefuzz_sample.csv");
+            //return new VirtualFileResult("sciencefuzz_sample.csv", "text/csv");
+            //return new PhysicalFileResult("sciencefuzz_sample.csv", "text/csv");
+        }
+    }
+}
