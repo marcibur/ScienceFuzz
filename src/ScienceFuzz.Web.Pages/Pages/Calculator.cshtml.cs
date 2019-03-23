56fbda1111b269eb2da8371096ceb50a95d4fec1
Marcin Burak <marcin.burak@outlook.com>
2019-03-23 20:30:07 +0100

    CSV file parsing

--- src/ScienceFuzz.Web.Pages/Pages/Calculator.cshtml.cs
+++ src/ScienceFuzz.Web.Pages/Pages/Calculator.cshtml.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CsvHelper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using ScienceFuzz.Web.Pages.Tools;
+using System.IO;
+using System.Linq;
 
 namespace ScienceFuzz.Web.Pages.Pages
 {
@@ -18,12 +21,29 @@ namespace ScienceFuzz.Web.Pages.Pages
             ResultsJSON = JsonConvert.SerializeObject(new double[] { });
         }
 
+        public class CsvRow
+        {
+            public string Journal { get; set; }
+            public double Social { get; set; }
+            public double Agriculture { get; set; }
+            public double Exact { get; set; }
+            public double Medicine { get; set; }
+            public double Humanities { get; set; }
+            public double Technology { get; set; }
+            public double Natural { get; set; }
+            public double Arts { get; set; }
+            public int Contributions { get; set; }
+        }
+
         public IActionResult OnPost()
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid || CsvData != null)
             {
-                var t = CsvData;
-
+                using (var reader = new StreamReader(CsvData.OpenReadStream()))
+                using (var csv = new CsvReader(reader))
+                {
+                    var journals = csv.GetRecords<CsvRow>().ToList();
+                }
             }
 
             return Page();
