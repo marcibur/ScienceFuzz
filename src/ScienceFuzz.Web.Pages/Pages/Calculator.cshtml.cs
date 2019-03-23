3a1a3fc154df29e42d7e29040716ac37ea8c964d
Marcin Burak <marcin.burak@outlook.com>
2019-03-23 19:48:53 +0100

    File upload form correction

--- src/ScienceFuzz.Web.Pages/Pages/Calculator.cshtml.cs
+++ src/ScienceFuzz.Web.Pages/Pages/Calculator.cshtml.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using ScienceFuzz.Web.Pages.Tools;
 
 namespace ScienceFuzz.Web.Pages.Pages
 {
     public class CalculatorModel : PageModel
     {
+        [BindProperty(SupportsGet = false)]
+        [ValidFileExtensions("csv")]
+        public IFormFile CsvData { get; set; }
         public string ResultsJSON { get; private set; }
 
         public void OnGet()
         {
             ResultsJSON = JsonConvert.SerializeObject(new double[] { });
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var t = CsvData;
+
+            }
+
+            return Page();
         }
     }
 }
