f12973206bf70f1f15902a0ef58ffbdcd7876715
Marcin Burak <marcin.burak@outlook.com>
2019-03-23 13:41:15 +0100

    MVC initial frontend

---
+++ src/ScienceFuzz.Web.Pages/Pages/Calculator.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScienceFuzz.Web.Pages.Pages
+{
+    public class CalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
