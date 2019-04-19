dcdd04045a21e10b9782f0d6472dfcb62fa1cb73
Marcin Burak <marcin.burak@outlook.com>
2019-04-19 17:26:47 +0200

    Added T-SNE info page

--- src/ScienceFuzz.Web.Pages/Pages/TSNE.cshtml.cs
+++ src/ScienceFuzz.Web.Pages/Pages/TSNE.cshtml.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ScienceFuzz.Web.Pages.Pages
 {
     public class TSNEModel : PageModel
     {
+        public bool Calculate { get; set; } = false;
+
         public void OnGet()
         {
 
+        }
+
+        public void OnPost()
+        {
+            Calculate = true;
         }
     }
 }
