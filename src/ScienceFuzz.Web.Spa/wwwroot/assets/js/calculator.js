56c9f2389e4da1db6493154a9945c1c67a9b1735
Marcin Burak <29354406+marcibur@users.noreply.github.com>
2019-11-11 20:26:43 +0100

    Calculator view

--- src/ScienceFuzz.Web.Spa/wwwroot/assets/js/calculator.js
+++ src/ScienceFuzz.Web.Spa/wwwroot/assets/js/calculator.js
@@ -1,5 +1,36 @@
-    var ctx = document.getElementById("results-chart");
-    var myChart = new Chart(ctx, {
+function initCharts() {
+    var ctx = document.getElementById('chart-disciplines-bars');
+    console.log(ctx);
+    new Chart(ctx, {
+        type: 'horizontalBar',
+        data: {
+            labels: ["Humanities", "Social", "Health", "Technology", "Exact", "Natural", "Agriculture", "Arts"],
+            datasets: [{
+                backgroundColor: [
+                    'rgba(13, 162, 0, .5)',
+                    'rgba(13, 162, 0, .5)',
+                    'rgba(13, 162, 0, .5)',
+                    'rgba(13, 162, 0, .5)',
+                    'rgba(13, 162, 0, .5)',
+                    'rgba(13, 162, 0, .5)',
+                    'rgba(13, 162, 0, .5)',
+                    'rgba(13, 162, 0, .5)'
+                ],
+                data: [1, 2, 3, 4, 5, 6, 7, 8]
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            tooltips: {
+                displayColors: false
+            }
+        }
+    });
+
+    ctx = document.getElementById('chart-domains-radar');
+    new Chart(ctx, {
         type: 'radar',
         data: {
             labels: ["Humanities", "Social", "Health", "Technology", "Exact", "Natural", "Agriculture", "Arts"],
@@ -18,8 +49,9 @@
             legend: {
                 display: false
             },
-            tooltips:{
+            tooltips: {
                 displayColors: false
             }
         }
     });
+}
