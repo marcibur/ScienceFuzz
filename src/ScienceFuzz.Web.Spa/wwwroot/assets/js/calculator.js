4cb633e87d72a1521b2afe7dc67e4b7958cba9d7
Marcin Burak <29354406+marcibur@users.noreply.github.com>
2019-11-11 19:26:30 +0100

    Added a SPA project

---
+++ src/ScienceFuzz.Web.Spa/wwwroot/assets/js/calculator.js
@@ -0,0 +1,25 @@
+    var ctx = document.getElementById("results-chart");
+    var myChart = new Chart(ctx, {
+        type: 'radar',
+        data: {
+            labels: ["Humanities", "Social", "Health", "Technology", "Exact", "Natural", "Agriculture", "Arts"],
+            datasets: [{
+                data: [1, 2, 3, 4, 5, 6, 7, 8],
+                backgroundColor: [
+                    'rgba(13, 162, 0, .5)'
+                ],
+                borderColor: [
+                    'rgb(10, 129, 0)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            tooltips:{
+                displayColors: false
+            }
+        }
+    });
