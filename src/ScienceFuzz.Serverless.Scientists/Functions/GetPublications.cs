fc579443632ea16a018ff311f9b70126fe5e79c7
Marcin Burak <29354406+marcibur@users.noreply.github.com>
2019-11-23 16:18:59 +0100

    Proxy has been setup

--- src/ScienceFuzz.Serverless.Scientists/Functions/GetPublications.cs
+++ src/ScienceFuzz.Serverless.Scientists/Functions/GetPublications.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace ScienceFuzz.Serverless.Scientists
 {
+    // TODO: Refactor
     public static class GetPublications
     {
         [FunctionName(nameof(GetPublications))]
